a12a5e9275e0d61523956f11306bac3e8ae6b82c
dieromeo <diegoromeo99@icloud.com>
2023-11-30 18:44:17 +0100

    fix: message hour

--- milestone4/js/main.js
+++ milestone4/js/main.js
@@ -22,17 +22,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['10/01/2020', '15:30', ':55'],
+                            date: { day: '10/01/2020', currentHour: '15:30', second: ':55' },
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: ['10/01/2020', '15:50', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:50', second: ':00' },
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: ['10/01/2020', '16:15', ':22'],
+                            date: { day: '10/01/2020', currentHour: '16:15', second: ':22' },
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -44,17 +44,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['20/03/2020', '16:30', ':00'],
+                            date: { day: '20/03/2020', currentHour: '16:30', second: ':00' },
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: ['20/03/2020', '16:30', ':55'],
+                            date: { day: '20/03/2020', currentHour: '16:30', second: ':55' },
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: ['20/03/2020', '16:35', ':00'],
+                            date: { day: '20/03/2020', currentHour: '16:35', second: ':00' },
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -66,17 +66,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['28/03/2020', '10:10', ':40'],
+                            date: { day: '28/03/2020', currentHour: '10:10', second: ':40' },
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: ['28/03/2020', '10:20', ':10'],
+                            date: { day: '28/03/2020', currentHour: '10:20', second: ':10' },
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: ['28/03/2020', '16:15', ':22'],
+                            date: { day: '28/03/2020', currentHour: '16:15', second: ':22' },
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -88,12 +88,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['10/01/2020', '15:30', ':55'],
+                            date: { day: '10/01/2020', currentHour: '15:30', second: ':55' },
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: ['10/01/2020', '15:50', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:50', second: ':00' },
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -105,12 +105,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['10/01/2020', '15:30', ':55'],
+                            date: { day: '10/01/2020', currentHour: '15:30', second: ':55' },
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: ['10/01/2020', '15:50', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:50', second: ':00' },
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -122,17 +122,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['10/01/2020', '15:30', ':55'],
+                            date: { day: '10/01/2020', currentHour: '15:30', second: ':55' },
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: ['10/01/2020', '15:50', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:50', second: ':00' },
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: ['10/01/2020', '15:51', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:51', second: ':00' },
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -144,12 +144,12 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['10/01/2020', '15:30', ':55'],
+                            date: { day: '10/01/2020', currentHour: '15:30', second: ':55' },
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: ['10/01/2020', '15:50', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:50', second: ':00' },
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -161,17 +161,17 @@ createApp({
                     visible: true,
                     messages: [
                         {
-                            date: ['10/01/2020', '15:30', ':55'],
+                            date: { day: '10/01/2020', currentHour: '15:30', second: ':55' },
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: ['10/01/2020', '15:50', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:50', second: ':00' },
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: ['10/01/2020', '15:51', ':00'],
+                            date: { day: '10/01/2020', currentHour: '15:51', second: ':00' },
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -198,9 +198,9 @@ createApp({
         },
         sendMessage() {
             if (this.userText.trim() !== '') {
-                this.contacts[this.currentIndex].messages.push({ date: this.hour, message: this.userText, status: 'sent' });
+                this.contacts[this.currentIndex].messages.push({ date: { day: '', currentHour: this.hour, second: '' }, message: this.userText, status: 'sent' });
                 this.userText = '';
-                setTimeout(() => { this.contacts[this.currentIndex].messages.push({ date: this.hour, message: 'Ok!', status: 'received' }) }, 1000);
+                setTimeout(() => { this.contacts[this.currentIndex].messages.push({ date: { day: '', currentHour: this.hour, second: '' }, message: 'Ok!', status: 'received' }) }, 1000);
             }
         },
         searchedChat() {
